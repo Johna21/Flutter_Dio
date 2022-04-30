b628175fe9aa570020b468f318480bfde9398266
Johna21 <58895485+Johna21@users.noreply.github.com>
2022-04-30 12:41:25 +0300

    updated

--- json_serializeable/lib/dio/dio_client.dart
+++ json_serializeable/lib/dio/dio_client.dart
@@ -15,7 +15,60 @@ class DioClient{
     try{
       final response = await _dio.get(_postendpoint +"/$id");
       debugPrint(response.toString());
+      return Data.fromJson(response.data);
+    }on DioError catch(e){
+      if(e.response !=null){
+        print('Dio error!');
+      print('STATUS: ${e.response?.statusCode}');
+      print('DATA: ${e.response?.data}');
+      print('HEADERS: ${e.response?.headers}');
+      }else{
+        print('Error sending request!');
+      print(e.message);
+      }
+    }
+    return user;
+  }
 
+  Future<Data?> createUser({required int userId,required String title,required String body}) async{
+    Data? user;
+    try{
+      final response = await _dio.post(
+        _postendpoint,
+          data: {
+            "userId":userId,
+            "title":title,
+            "body":body
+          }
+        );
+      debugPrint(response.toString());
+      return Data.fromJson(response.data);
+    }on DioError catch(e){
+      if(e.response !=null){
+        print('Dio error!');
+      print('STATUS: ${e.response?.statusCode}');
+      print('DATA: ${e.response?.data}');
+      print('HEADERS: ${e.response?.headers}');
+      }else{
+        print('Error sending request!');
+      print(e.message);
+      }
+    }
+    return user;
+  }
+
+  Future<Data?> updateUser({required postId,required int userId,required String title,required String body}) async{
+    Data? user;
+    try{
+      final response = await _dio.put(
+        _postendpoint + "/$postId",
+          data: {
+            "userId":userId,
+            "title":title,
+            "body":body
+          }
+        );
+      debugPrint(response.toString());
       return Data.fromJson(response.data);
     }on DioError catch(e){
       if(e.response !=null){
--- json_serializeable/lib/screen/home.dart
+++ json_serializeable/lib/screen/home.dart
@@ -1,6 +1,3 @@
-import 'dart:ffi';
-import 'dart:html';
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:json_serializeable/dio/dio_client.dart';
@@ -8,27 +5,38 @@ import 'package:json_serializeable/model/data.dart';
 import 'package:json_serializeable/model/user.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   late DioClient _dioClient;
-  late Future<Data?> post;
-
+  int requesting = 0;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _dioClient = DioClient();
   }
 
   @override
   Widget build(BuildContext context) {
-     
+    Future<Data?> post = _dioClient.getUser(id: 1);
+    Future<Data?> createpost =
+        _dioClient.createUser(
+          userId: 1, 
+          title: "title", 
+          body: "body");
+    Future<Data?> updatepost =
+        _dioClient.updateUser(
+          postId: 1,
+          userId: 1, 
+          title: "updated", 
+          body: "body"
+          );
+
     return Scaffold(
       appBar: AppBar(
         title: Text("User Info"),
@@ -36,38 +44,103 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
           Center(
-            child: FutureBuilder<Data?>(
-              future: post,
-              builder: (context,snapshot){
-                if(snapshot.hasData){
-                  
-                    return Column(
-                        mainAxisSize: MainAxisSize.min,
+            child: Column(
+              children: [
+                if (requesting == 1) ...[
+                  FutureBuilder<Data?>(
+                    future: post,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(snapshot.data!.title),
+                            SizedBox(height: 8.0),
+                          ],
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text("${snapshot.hasError}");
+                      }
 
-                      children: [
-                        Text(snapshot.data!.title),
-                        SizedBox(height: 8.0),
-                        
-                      ],
-                    
-                    );
-                  
-                }else if(snapshot.hasError){
-                  return Text("${snapshot.hasError}");
-                }
-                return CircularProgressIndicator();
-              },
+                      return Container();
+                    },
+                  ),
+                ] else if (requesting == 2) ...[
+                  FutureBuilder<Data?>(
+                    future: createpost,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(snapshot.data!.userId.toString()),
+                            SizedBox(height: 8.0),
+                            Text(snapshot.data!.title),
+                            SizedBox(height: 8.0),
+                            Text(snapshot.data!.body),
+                          ],
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text("${snapshot.hasError}");
+                      }
+
+                      return Container();
+                    },
+                  ),
+                ] else if (requesting == 3) ...[
+                  FutureBuilder<Data?>(
+                    future: updatepost,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(snapshot.data!.userId.toString()),
+                            SizedBox(height: 8.0),
+                            Text(snapshot.data!.title),
+                            SizedBox(height: 8.0),
+                            Text(snapshot.data!.body),
+                          ],
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text("${snapshot.hasError}");
+                      }
+
+                      return Container();
+                    },
+                  ),
+                ]
+              ],
             ),
           ),
-          ElevatedButton(
-            onPressed: (){
-              post = _dioClient.getUser(id: 1);
-            }, 
-            child: Text("Get Post")
-            )
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      requesting = 1;
+                    });
+                  },
+                  child: Text("Get Post")),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      requesting = 2;
+                    });
+                  },
+                  child: Text("Create Post")),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      requesting = 3;
+                    });
+                  },
+                  child: Text("Update Post")),
+            ],
+          )
         ],
       ),
     );
   }
-  
 }
